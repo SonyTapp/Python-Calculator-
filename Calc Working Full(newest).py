09e096d2a2819b949162c724be5f8b54d557ad34
SonyTapp <165155109+SonyTapp@users.noreply.github.com>
2024-05-20 10:25:56 +0200

    Add files via upload
    This is the latest version of my python calculator. This calculator is aimed at primary school children, allowing them to do basic calculations using (+,-,*,/), as well as some more options for more advanced students, such as to the power of, square root, and squared. 
    
    So far, the calculator does not have a UI and relies on terminal input and output. You can run it using the terminal in Visual Studio Code or any other terminal that has Python installed.

---
+++ Calc Working Full(newest).py
@@ -0,0 +1,136 @@
+import math
+result = 0  # Initialize the sum total
+calc = ''   # Initialize the list of sums shown 
+
+print('--------------------------')
+print('Welcome!')
+print('--------------------------')
+print('Table of Mathematical Operations:')
+print('--------------------------')
+print('|   Addition         +    |')           # printed welcome message 
+print('|   Subtraction      -    |')
+print('|   Multiplication   *    |')
+print('|   Division         /    |')
+print('|   Power Of         P    |')
+print('|   Squared          Sq   |')
+print('|   Square Root      Sr   |')
+print('| --Reset--          R    |')
+print('| --Exit Program--   E    |')
+print('--------------------------')
+
+
+while True: #overall continous loop until break with E (whole calculator)
+    print('-----------------------------------------')
+    operation = input("Choose Operation (+, -, *, /, P, Sq, Sr, R, E ): ")
+    print('--------------------------')
+    
+
+    while operation not in ('+', '-', '*', '/', 'E', 'P', 'R','Sq','Sr'):       #while o not in --- means loop will not end until o is equal to one of the tuple       
+        print('Invalid input, Choose an Operator   --->   +, -, *, /, or E to Exit')
+        print('--------------------------')
+        operation = input('Choose Operation: ') 
+        print('--------------------------') 
+
+    if operation == 'R': #Resets calc
+        result = 0  # resets total and sums and list of sums back to 0 and continues back to the start of the loop
+        calc = '' 
+        print('calculator Reset!') 
+        print('--------------------------') 
+        continue 
+    if operation == 'E': #exits the calc
+        result = 0
+        calc = '' 
+        print('calculator ShutDown') 
+        print('--------------------------') 
+        break # ends nearest enclosing loop (whole calc)    
+
+    if operation == 'Sq':      # start of Sqaure 
+        if result == 0 and calc == '':             # if first time using                            
+            num1 = float(input('Choose Your Number: '))             # choose number to square.
+            result = num1 * num1            
+            calc += f"\n     {operation}   {num1} = {result}"     # add sum to the list (first entry)
+            print(calc)
+            continue 
+        else:
+            result2 = result * result   # if it's not the first sum a new variable is needed(i think) it enables me to have the previus result and the new result in the same 'sum line' 
+            calc += f"\n     {operation}   {result} = {result2}"     # adds sum to the list
+            print(calc)
+            result = result2       # this makes it so that more sums can be performed with the new answer from result 2
+            continue 
+
+
+
+    if operation == 'Sr':   # square root option.   Works the same way as above.
+        if result == 0 and calc == '':             # if first time using    
+            num1 = float(input('Choose Your Number: '))
+            result =  math.sqrt(num1) 
+            calc += f"\n    {operation}   {num1} = {result}"
+            print(calc)
+            continue     
+        else:
+            result2 = math.sqrt(result)        # if it's not the first sum
+            calc += f"\n     {operation}   {result} = {result2}"
+            print(calc)
+            result = result2
+            continue
+
+
+    if result == 0 and calc == '':        # for main operators choose first number the first time of use.    # Updated to not reset if Sum = 0
+        while True:                                                       
+            try:                                                           
+                num1 = float(input('Choose Your First Number: '))   # first number choice (if Sr or Sq arent chosen)
+                print('--------------------------')
+                break                                                      
+
+            except ValueError:             # check to make sure number is valid number                              
+                print('--------------------------')
+                print('Invalid input! Please Enter a Valid Number!')
+                print('--------------------------')
+    else:
+        num1 = result   # makes continuous sums by making answer of previous sum num1 for next sum.     
+
+    
+    while True:                                                       
+        try:                                                           
+            num2 = float(input('Choose Your Next Number: '))    #second number choice
+            print('--------------------------')
+            break                                                    
+
+        except ValueError:                                    # check to make sure number is valid number            
+            print('--------------------------')
+            print('Invalid input! Please Enter a Valid Number!')
+            print('--------------------------')                # IT MAY BE POSSIBLE TO COMBINE THE TWO ERROR CHECKS ON THE TWO INPUTS
+
+
+
+    if operation == "+":
+        result = num1 + num2    # basic calculator operators 
+    elif operation == "-":
+        result = num1 - num2
+    elif operation == "*":
+        result = num1 * num2
+    elif operation == "/":
+        if num2 != 0:     # check if num2 IS EXPLICITLY NOT 0 and the else is if it is, better for errors apparently but could be reversed to if num2 = 0 etc
+            result = num1 / num2
+        else:
+            print("Error: Division by zero!")
+            continue
+    elif operation == 'P':
+        result = pow(num1, num2)   # does one number to the power of another using the .math expression pow(x, y)
+       
+
+
+          
+
+    if calc:    # if calc has something in it already, you already did a sum it only shows OP + new number = result
+        calc += f"\n     {operation}    {num2} = {result}"  
+    else:
+        calc = f"{num1} {operation}    {num2} = {result}"   # if first time it shows N1 OP N2 = Rsult       
+    
+
+    print(calc) # this print keeps stacking with above \n
+    
+
+
+
+
